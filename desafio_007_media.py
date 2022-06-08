2d767e4104f7e62d9db3dc5f3f4ed8aeb9796967
yuriaugusto <ya.augusto@gmail.com>
2022-06-07 22:58:11 -0300

    change name of file

---
+++ desafio_007_media.py
@@ -0,0 +1,6 @@
+print('Desafio 007:\nDesenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.')
+
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+
+print(f'A média do aluno é {(nota1 + nota2) / 2}.')
